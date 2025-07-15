324f81b3e4fa8ef39c2b14807fbf9fb30d3e6be8
Lívia Silveira da Rosa <livia.rosa92@gmail.com>
2025-07-15 19:39:25 -0300

    Exercício 3-a: cálculo da média

---
+++ exercicio-3-a.js
@@ -0,0 +1,8 @@
+// DESAFIOS A
+
+// a) Calcule a média de 3 números: 15, 25 e 38
+const num1 = 15;
+const num2 = 25;
+const num3 = 38;
+const media = (num1 + num2 + num3) / 3;
+console.log("Média de 15, 25 e 38:", media);
