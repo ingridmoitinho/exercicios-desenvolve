406b15f1409d62de6ba7e579e70309598d1727f5
Lívia Silveira da Rosa <livia.rosa92@gmail.com>
2025-07-15 19:40:59 -0300

    Exercício 3-c: arredondamento de número

---
+++ exercicio-3-c.js
@@ -0,0 +1,6 @@
+// DESAFIO C
+
+// c) Arredonde o resultado de 9.7 dividido por 2 para o inteiro mais próximo
+const resultado = 9.7 / 2;
+const arredondado = Math.round(resultado);
+console.log("9.7 dividido por 2 arredondado:", arredondado);
