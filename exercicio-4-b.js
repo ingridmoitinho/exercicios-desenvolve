007b3cddeaf31a92961252c807ae0f3125dfa21b
ingridmoitinho <ingridmoitinho@gmail.com>
2025-07-16 20:04:18 -0300

    Adiciona resolução do exercício 4b

---
+++ exercicio-4-b.js
@@ -0,0 +1,11 @@
+// Desafio b) Verifique se um número é par ou ímpar
+
+const numero = 22;
+
+console.log("Verificando o número:", numero);
+
+if (numero % 2 === 0) {
+    console.log("O número é PAR.");
+} else {
+    console.log("O número é ÍMPAR.");
+}
